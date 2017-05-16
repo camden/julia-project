4be83541177ef39af6eaf3c88ae14a57c12f42fc
Camden Bickel <cam@bickel.io>
2017-05-15 21:30:13 -0700

    basic api

--- src/server.js
+++ src/server.js
@@ -1,3 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
-console.log(express);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true  }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({message: 'what up!'})
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log(`Running on port ${port}`);
