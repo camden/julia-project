107ffd7c66f1f9146766749eba74209e9439b7e5
Camden Bickel <cam@bickel.io>
2017-05-22 19:10:48 -0700

    Added error handling

--- src/server.js
+++ src/server.js
@@ -1,41 +1,51 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+mongoose.Promise = global.Promise;
 
 import Submission from './models/submission';
 
 // Database
-mongoose.connect('mongodb://localhost:27017');
-
-// Server
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true  }));
-app.use(bodyParser.json());
-
-const port = process.env.PORT || 8080;
-
-const router = express.Router();
-
-router.route('/submissions')
-    .post((req, res) => {
-        const sub = new Submission();
-        sub.category = req.body.category;
-
-        sub.save((err) => {
-            if (err) {
-                res.send(err);
-            }
-
-            res.json({ message: 'Submission created!' });
-        })
-    });
-
-router.get('/', (req, res) => {
-    res.json({message: 'what up!'})
+console.log("Connecting to MongoDB...")
+mongoose.connect('mongodb://localhost:27017/julia-server').then(() => {
+    console.log("Connected to MongoDB.")
+    main();
+}).catch((err) => {
+    console.error("Could not connect to MongoDB. Is it running on port 27017?");
+    process.exit(1);
 });
 
-app.use('/api', router);
+function main() {
+    // Server
+    const app = express();
 
-app.listen(port);
-console.log(`Running on port ${port}`);
+    app.use(bodyParser.urlencoded({ extended: true  }));
+    app.use(bodyParser.json());
+
+    const port = process.env.PORT || 8080;
+
+    const router = express.Router();
+
+    router.route('/submissions')
+        .post((req, res) => {
+            const sub = new Submission();
+            sub.category = req.body.category;
+
+            sub.save((err) => {
+                if (err) {
+                    res.send(err);
+                }
+
+                res.json({ message: 'Submission created!' });
+            })
+        });
+
+    router.get('/', (req, res) => {
+        res.json({message: 'what up!'})
+    });
+
+    app.use('/api', router);
+
+    app.listen(port);
+    console.log(`Running on port ${port}`);
+}
