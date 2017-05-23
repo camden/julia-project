0601b813a7b49236893789b7cce8f12d18c6597a
Camden Bickel <cam@bickel.io>
2017-05-22 22:37:08 -0700

    fixed cors + package json redundancy

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise;
 
@@ -8,55 +9,56 @@ import Submission from './models/submission';
 // Database
 console.log("Connecting to MongoDB...")
 mongoose.connect('mongodb://localhost:27017/julia-server').then(() => {
-    console.log("Connected to MongoDB.")
-    main();
+  console.log("Connected to MongoDB.")
+  main();
 }).catch((err) => {
-    console.error("Could not connect to MongoDB. Is it running on port 27017?");
-    process.exit(1);
+  console.error("Could not connect to MongoDB. Is it running on port 27017?");
+  process.exit(1);
 });
 
 function main() {
-    // Server
-    const app = express();
+  // Server
+  const app = express();
 
-    app.use(bodyParser.urlencoded({ extended: true  }));
-    app.use(bodyParser.json());
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true  }));
+  app.use(bodyParser.json());
 
-    const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8080;
 
-    const router = express.Router();
+  const router = express.Router();
 
-    router.route('/submissions')
-        .post((req, res) => {
-            const sub = new Submission();
-            sub.category = req.body.category;
-            sub.content = req.body.content;
-            sub.authorName = req.body.authorName;
+  router.route('/submissions')
+    .post((req, res) => {
+      const sub = new Submission();
+      sub.category = req.body.category;
+      sub.content = req.body.content;
+      sub.authorName = req.body.authorName;
 
-            sub.save((err, submission) => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.json(submission);
-                }
-            })
-        })
-        .get((req, res) => {
-            Submission.find((err, submissions) => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.json(submissions);
-                }
-            })
-        });
-
-    router.get('/', (req, res) => {
-        res.json({message: 'what up!'})
+      sub.save((err, submission) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(submission);
+        }
+      })
+    })
+    .get((req, res) => {
+      Submission.find((err, submissions) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(submissions);
+        }
+      })
     });
 
-    app.use('/api', router);
+  router.get('/', (req, res) => {
+    res.json({message: 'what up!'})
+  });
 
-    app.listen(port);
-    console.log(`Running on port ${port}`);
+  app.use('/api', router);
+
+  app.listen(port);
+  console.log(`Running on port ${port}`);
 }
