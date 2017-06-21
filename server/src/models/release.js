ba2c78d7af6f3bd6e04cb5ccb481c2e1b276e7fd
Camden Bickel <cam@bickel.io>
2017-06-20 22:24:54 -0700

    fixed indentation

--- server/src/models/release.js
+++ server/src/models/release.js
@@ -4,39 +4,33 @@ import autoIncrement from 'mongoose-sequence';
 import SubmissionSchema from './submission';
 
 const ReleaseSchema = new mongoose.Schema({
-    previewBeginDate: {
-        type: Date,
-        required: true
-    },
-    prodBeginDate: {
-      type: Date,
-      required: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    contentTitle: {
-        type: String,
-        required: true
-    },
-    rawContentWithoutTitle: {
-        type: mongoose.Schema.Types.Mixed,
-        required: true
-    },
-    authorName: {
-        type: String,
-        required: true
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  submissions: {
+    type: [SubmissionSchema],
+    default: [],
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['major', 'minor'],
+    required: true
+  },
+  previewBeginDate: {
+    type: Date,
+    required: true
+  },
+  prodBeginDate: {
+    type: Date,
+    required: true
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
 });
 
 ReleaseSchema.plugin(autoIncrement, {inc_field: 'id'});
--- server/src/models/submission.js
+++ server/src/models/submission.js
@@ -2,31 +2,31 @@ import mongoose from 'mongoose';
 import autoIncrement from 'mongoose-sequence';
 
 const SubmissionSchema = new mongoose.Schema({
-    category: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    contentTitle: {
-        type: String,
-        required: true
-    },
-    rawContentWithoutTitle: {
-        type: mongoose.Schema.Types.Mixed,
-        required: true
-    },
-    authorName: {
-        type: String,
-        required: true
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-        required: true
-    }
+  category: {
+    type: String,
+      required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  contentTitle: {
+    type: String,
+    required: true
+  },
+  rawContentWithoutTitle: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true
+  },
+  authorName: {
+    type: String,
+    required: true
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
 });
 
 SubmissionSchema.plugin(autoIncrement, {inc_field: 'id'});
